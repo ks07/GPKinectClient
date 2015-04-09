af57e278f4ea8d2649dc055d02757aa723265ca3
AHarman <ah12819@my.bristol.ac.uk>
2015-04-09 20:04:29 +0100

    Removed scanner things from main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include "main.h"
 #include "KinectInterface.h"
-#include "OpenARScanner.h"
 
 int main(int argc, char* argv[])
 {
-	OpenARScanner* scanner = new OpenARScanner();
-	scanner->openARLoop();
-
 	if (argc != 3)
 	{
 		std::cerr << "Usage: client <host> <port>" << std::endl;
 		return 1;
 	}
-	printf("Here!\n");
 	OCVSlaveProtocol client(argv[1], argv[2]);
 	
 	client.Connect();
